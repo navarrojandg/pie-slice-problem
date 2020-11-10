fecf070feead0791caf7351e8e2148de7ab5ab98
navarrojandg <42248628+navarrojandg@users.noreply.github.com>
2020-11-10 11:59:37 -0800

    feat/ checking puzzle2

--- py/main.py
+++ py/main.py
@@ -5,6 +5,15 @@ from lib.slice import Slice
 if __name__ == "__main__":
   puzzles = stacksFromFile('../dataset.txt')
 
+  print('1st round')
   print(solve(puzzles['puzzle2']))
   print()
   print(puzzles['puzzle2'])
+  print()
+
+  print('2nd round')
+  puzzles['puzzle2'].reset()
+  print(solve(puzzles['puzzle2']))
+  print()
+  print(puzzles['puzzle2'])
+  print()
