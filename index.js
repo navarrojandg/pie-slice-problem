1beeda657d0a43240edc19ef5f4ca744e68d3dfd
navarrojandg <42248628+navarrojandg@users.noreply.github.com>
2020-11-24 14:42:14 -0800

    refactored generators, abstracted them to their own classes

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const path = require('path');
 const yargs = require('yargs/yargs');
 const argv = yargs(process.argv).argv;
 const { colors, slices, repeats } = argv;
-const {generator1} = require('./lib/generators/one');
-const {generator2} = require('./lib/generators/two');
-const {generator3} = require('./lib/generators/three');
-const {generator4} = require('./lib/generators/four');
+const generator1 = new (require('./lib/generators/one'));
+const generator2 = new (require('./lib/generators/two'));
+const generator3 = new (require('./lib/generators/three'));
+const generator4 = new (require('./lib/generators/four'));
 
 const filename = path.join(__dirname, 'dataset.txt'); 
 const puzzles = {
@@ -18,15 +18,15 @@ const puzzles = {
 
 for (let i = 1; i <= colors; i++) {
   if (i % 3 == 1) {
-    puzzles.puzzle1.unshift([generator1(i)]);
-    puzzles.puzzle2.unshift([generator2(i)]);
-    puzzles.puzzle3.unshift([generator3(i)]);
-    puzzles.puzzle4.unshift([generator4(i)]);
+    puzzles.puzzle1.unshift([generator1.generate()]);
+    puzzles.puzzle2.unshift([generator2.generate()]);
+    puzzles.puzzle3.unshift([generator3.generate()]);
+    puzzles.puzzle4.unshift([generator4.generate()]);
   } else {
-    puzzles.puzzle1[0].push(generator1(i));
-    puzzles.puzzle2[0].push(generator2(i));
-    puzzles.puzzle3[0].push(generator3(i));
-    puzzles.puzzle4[0].push(generator4(i));
+    puzzles.puzzle1[0].push(generator1.generate());
+    puzzles.puzzle2[0].push(generator2.generate());
+    puzzles.puzzle3[0].push(generator3.generate());
+    puzzles.puzzle4[0].push(generator4.generate());
   };
   if (i % 3 == 0) {
     // take the previous value and transform to string
