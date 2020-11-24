09ed9f5d77ef2e14d4525fc31428b39f9136f0ac
navarrojandg <42248628+navarrojandg@users.noreply.github.com>
2020-11-24 15:34:30 -0800

    feat/ refactored puzzle keys to read more verbose

--- index.js
+++ index.js
@@ -10,38 +10,38 @@ const generator4 = new (require('./lib/generators/four'));
 
 const filename = path.join(__dirname, 'dataset.txt'); 
 const puzzles = {
-  puzzle1: [],
-  puzzle2: [],
-  puzzle3: [],
-  puzzle4: [],
+  one: [],
+  two: [],
+  three: [],
+  four: [],
 };
 
 for (let i = 1; i <= colors; i++) {
   if (i % 3 == 1) {
-    puzzles.puzzle1.unshift([generator1.generate()]);
-    puzzles.puzzle2.unshift([generator2.generate()]);
-    puzzles.puzzle3.unshift([generator3.generate()]);
-    puzzles.puzzle4.unshift([generator4.generate()]);
+    puzzles.one.unshift([generator1.generate()]);
+    puzzles.two.unshift([generator2.generate()]);
+    puzzles.three.unshift([generator3.generate()]);
+    puzzles.four.unshift([generator4.generate()]);
   } else {
-    puzzles.puzzle1[0].push(generator1.generate());
-    puzzles.puzzle2[0].push(generator2.generate());
-    puzzles.puzzle3[0].push(generator3.generate());
-    puzzles.puzzle4[0].push(generator4.generate());
+    puzzles.one[0].push(generator1.generate());
+    puzzles.two[0].push(generator2.generate());
+    puzzles.three[0].push(generator3.generate());
+    puzzles.four[0].push(generator4.generate());
   };
   if (i % 3 == 0) {
     // take the previous value and transform to string
-    puzzles.puzzle1[0] = puzzles.puzzle1[0].join(',');
-    puzzles.puzzle2[0] = puzzles.puzzle2[0].join(',');
-    puzzles.puzzle3[0] = puzzles.puzzle3[0].join(',');
-    puzzles.puzzle4[0] = puzzles.puzzle4[0].join(',');
+    puzzles.one[0] = puzzles.one[0].join(',');
+    puzzles.two[0] = puzzles.two[0].join(',');
+    puzzles.three[0] = puzzles.three[0].join(',');
+    puzzles.four[0] = puzzles.four[0].join(',');
   };
 };
 
 // reverse the order of the puzzles
-puzzles.puzzle1 = puzzles.puzzle1.reverse();
-puzzles.puzzle2 = puzzles.puzzle2.reverse();
-puzzles.puzzle3 = puzzles.puzzle3.reverse();
-puzzles.puzzle4 = puzzles.puzzle4.reverse();
+puzzles.one = puzzles.one.reverse();
+puzzles.two = puzzles.two.reverse();
+puzzles.three = puzzles.three.reverse();
+puzzles.four = puzzles.four.reverse();
 
 fs.writeFileSync(filename, JSON.stringify(puzzles, null, 2), 'utf8');
 console.log(`output puzzle dataset for ${colors} colors and ${slices} slices and ${repeats} repeats to:\n${filename}`);
