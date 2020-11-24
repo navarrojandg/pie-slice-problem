2f67d98ee24755d0be89079ee93ee1683f43a789
navarrojandg <42248628+navarrojandg@users.noreply.github.com>
2020-11-24 15:35:05 -0800

    feat/ refacted stack and moved unnecessary util functions to separate files

--- py/lib/stack.py
+++ py/lib/stack.py
@@ -1,9 +1,6 @@
 #array of slices
 from typing import List
-from typing import Dict
 from lib.slice import Slice
-import os
-import json
 
 class Stack:
   def __init__(self):
@@ -58,46 +55,3 @@ class Stack:
   def reset(self):
     for s in self.slices:
       s.reset()
-
-def stacksFromFile(filename) -> Dict[str, Stack]:
-  dirname = os.path.dirname(os.path.realpath('__file__'))
-  filepath = os.path.join(dirname, filename)
-  
-  with open(filepath) as f:
-    dataset = json.load(f)
-    return {
-      'puzzle1': Stack().fromArray(dataset['puzzle1']),
-      'puzzle2': Stack().fromArray(dataset['puzzle2']),
-      'puzzle3': Stack().fromArray(dataset['puzzle3']),
-      'puzzle4': Stack().fromArray(dataset['puzzle4'])
-    }
-
-def stackSliceAndValidate(_slice: Slice, _stack: Stack):
-  _stack.push(_slice)
-  valid = _stack.validate()
-
-  while(_slice.rotations < 3 and not valid):
-    valid = rotateSliceAndValidate(_slice, _stack)
-  
-  return valid
-  
-def rotateSliceAndValidate(_slice:Slice, _stack:Stack):
-  _slice.rotate()
-  return _stack.validate()
-
-def allStackChecked(stack: Stack):
-  return all(map(lambda s: s.checked, stack))
-
-def solve(stack: Stack) -> Stack:
-  solutionStack = Stack()
-  solutionStack.push(stack.pop())
-
-  while(not allStackChecked(stack)):
-    valid = stackSliceAndValidate(stack.pop(), solutionStack)
-    if not valid:
-      _slice = solutionStack.pop(0)
-      if _slice.rotations == 3:
-        _slice.checked = True
-      stack.push(_slice)
-
-  return solutionStack
