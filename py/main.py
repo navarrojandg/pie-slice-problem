36d0e2b3c554034ab7b9b0417bf6a1382629d1ca
navarrojandg <42248628+navarrojandg@users.noreply.github.com>
2020-11-27 23:30:31 -0800

    Update main.py

--- py/main.py
+++ py/main.py
@@ -1,17 +1,28 @@
-from lib.solver import stacksFromFile
-from lib.solver import solve
 from lib.stack import Stack
 from lib.slice import Slice
+import lib.solutions.dualstack as dualstack
+from lib.circulararray import CircularArray 
+
+import os
+import json
+def circularArraysFromFile(filename):
+  dirname = os.path.dirname(os.path.realpath('__file__'))
+  filepath = os.path.join(dirname, filename)
+  
+  with open(filepath) as f:
+    dataset = json.load(f)
+    c1 = CircularArray(31).fromArray(dataset['one'])
+    c2 = CircularArray().fromArray(dataset['two'])
+    c3 = CircularArray().fromArray(dataset['three'])
+    c4 = CircularArray().fromArray(dataset['four'])
+
+    return {
+      'one': c1.toArray(),
+      'two': c2.toArray(),
+      'three': c3.toArray(),
+      'four': c4.toArray()
+    }
 if __name__ == "__main__":
-  puzzles = stacksFromFile('../dataset.txt')
-  puzzleSet = puzzles['one']
-  # puzzleSet = puzzles['two']
-  # puzzleSet = puzzles['three']
-  # puzzleSet = puzzles['four']
-
-  print('solution')
-  print(solve(puzzleSet))
-  print('\n')
-
-  print('obstacles')
-  print(puzzleSet)
+  for name, puzzle in dualstack.stacksFromFile('../dataset.txt').items():
+    print(f'Puzzle {name.capitalize()}')
+    dualstack.solve(puzzle)
